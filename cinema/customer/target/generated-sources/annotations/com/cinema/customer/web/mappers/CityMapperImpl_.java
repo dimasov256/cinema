66f90927463b9bff5d6c69de083cd20e62379f9c
dimasov256 <dima.dimasov256@gmail.com>
2023-06-02 12:07:18 +0300

    Las commit

--- cinema/customer/target/generated-sources/annotations/com/cinema/customer/web/mappers/CityMapperImpl_.java
+++ cinema/customer/target/generated-sources/annotations/com/cinema/customer/web/mappers/CityMapperImpl_.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-05-28T03:36:06+0300",
+    date = "2023-06-02T07:30:57+0300",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.1 (Oracle Corporation)"
 )
 @Component
